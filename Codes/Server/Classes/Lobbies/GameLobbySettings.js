fe6e03c4d67344ed35f7a5f554623242f166fd8a
Lex <lexzh@outlook.com>
2024-10-15 14:34:51 -0700

    Delete Codes/Server directory

--- Codes/Server/Classes/Lobbies/GameLobbySettings.js
+++
@@ -1,7 +0,0 @@
-module.exports = class GameLobbySettings {
-    constructor(gameMode, maxPlayers, levelData) {
-        this.gameMode = 'No Gamemode Defined';
-        this.maxPlayers = maxPlayers;
-        this.levelData = levelData;
-    }
-}
